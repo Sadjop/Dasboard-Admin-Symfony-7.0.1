64315c06f0e00b8ed907a6dbff12d7553265fa3e
sadjop_ <arthur.maison.pro@gmail.com>
2024-02-14 10:47:25 +0100

    Create product + CRUD

---
+++ src/Entity/Product.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ProductRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ProductRepository::class)]
+class Product
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $product_title = null;
+
+    #[ORM\Column(type: Types::TEXT, nullable: true)]
+    private ?string $product_description = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $thumbnail_product = null;
+
+    #[ORM\Column(type: Types::DATE_MUTABLE)]
+    private ?\DateTimeInterface $product_create = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?float $average_note = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getProductTitle(): ?string
+    {
+        return $this->product_title;
+    }
+
+    public function setProductTitle(string $product_title): static
+    {
+        $this->product_title = $product_title;
+
+        return $this;
+    }
+
+    public function getProductDescription(): ?string
+    {
+        return $this->product_description;
+    }
+
+    public function setProductDescription(?string $product_description): static
+    {
+        $this->product_description = $product_description;
+
+        return $this;
+    }
+
+    public function getThumbnailProduct(): ?string
+    {
+        return $this->thumbnail_product;
+    }
+
+    public function setThumbnailProduct(?string $thumbnail_product): static
+    {
+        $this->thumbnail_product = $thumbnail_product;
+
+        return $this;
+    }
+
+    public function getProductCreate(): ?\DateTimeInterface
+    {
+        return $this->product_create;
+    }
+
+    public function setProductCreate(\DateTimeInterface $product_create): static
+    {
+        $this->product_create = $product_create;
+
+        return $this;
+    }
+
+    public function getAverageNote(): ?float
+    {
+        return $this->average_note;
+    }
+
+    public function setAverageNote(?float $average_note): static
+    {
+        $this->average_note = $average_note;
+
+        return $this;
+    }
+}
---
+++ src/Form/ProductType.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Product;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ProductType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('product_title')
+            ->add('product_description')
+            ->add('thumbnail_product')
+            ->add('product_create')
+            ->add('average_note')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Product::class,
+        ]);
+    }
+}
